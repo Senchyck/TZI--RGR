62168080f1510cce19ef8fdabe882765f8d5a790
Oleksandr Yavorskyi <sashayavorskiy05@gmail.com>
2024-10-27 22:37:32 +0200

    Add project files.

---
+++ TZI-RGR/TZI-RGR.cpp
@@ -0,0 +1,84 @@
+﻿#include <iostream>
+#include <openssl/evp.h>
+#include <openssl/rand.h>
+#include <cstring>
+
+void handleErrors() {
+    std::cerr << "Error occurred during encryption/decryption." << std::endl;
+    exit(1);
+}
+
+
+int encrypt(unsigned char* plaintext, int plaintext_len, unsigned char* key, unsigned char* iv, unsigned char* ciphertext) {
+    EVP_CIPHER_CTX* ctx = EVP_CIPHER_CTX_new();
+    if (!ctx) handleErrors();
+
+    int len;
+    int ciphertext_len;
+
+    if (1 != EVP_EncryptInit_ex(ctx, EVP_aes_128_cfb(), NULL, key, iv))
+        handleErrors();
+
+    if (1 != EVP_EncryptUpdate(ctx, ciphertext, &len, plaintext, plaintext_len))
+        handleErrors();
+    ciphertext_len = len;
+
+    if (1 != EVP_EncryptFinal_ex(ctx, ciphertext + len, &len))
+        handleErrors();
+    ciphertext_len += len;
+
+    EVP_CIPHER_CTX_free(ctx);
+    return ciphertext_len;
+}
+
+
+int decrypt(unsigned char* ciphertext, int ciphertext_len, unsigned char* key, unsigned char* iv, unsigned char* plaintext) {
+    EVP_CIPHER_CTX* ctx = EVP_CIPHER_CTX_new();
+    if (!ctx) handleErrors();
+
+    int len;
+    int plaintext_len;
+
+    if (1 != EVP_DecryptInit_ex(ctx, EVP_aes_128_cfb(), NULL, key, iv))
+        handleErrors();
+
+    if (1 != EVP_DecryptUpdate(ctx, plaintext, &len, ciphertext, ciphertext_len))
+        handleErrors();
+    plaintext_len = len;
+
+    if (1 != EVP_DecryptFinal_ex(ctx, plaintext + len, &len))
+        handleErrors();
+    plaintext_len += len;
+
+    EVP_CIPHER_CTX_free(ctx);
+    return plaintext_len;
+}
+
+int main() {
+    unsigned char key[16];
+    unsigned char iv[16];
+
+    if (!RAND_bytes(key, sizeof(key)) || !RAND_bytes(iv, sizeof(iv))) {
+        handleErrors();
+    }
+
+    unsigned char plaintext[] = "put maximum points please, I tried hard";
+    unsigned char ciphertext[128];
+    unsigned char decryptedtext[128];
+
+    int ciphertext_len = encrypt(plaintext, strlen((char*)plaintext), key, iv, ciphertext);
+
+    int decryptedtext_len = decrypt(ciphertext, ciphertext_len, key, iv, decryptedtext);
+
+    decryptedtext[decryptedtext_len] = '\0';
+
+    std::cout << "Original Text: " << plaintext << std::endl;
+    std::cout << "Encrypted Text: ";
+    for (int i = 0; i < ciphertext_len; i++) {
+        std::cout << std::hex << (int)ciphertext[i];
+    }
+    std::cout << std::endl;
+    std::cout << "Decrypted Text: " << decryptedtext << std::endl;
+
+    return 0;
+}
